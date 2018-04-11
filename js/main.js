3a33209d12d5225b6a3a6c1f6d30ae5a4505efce
Tim C <timoman8@hotmail.com>
2018-04-11 17:28:57 -0400

    .

--- js/main.js
+++ js/main.js
@@ -1,118 +1,173 @@
-Array.prototype.multipart = function(name) {
-  let rv = {};
-  for (let i = 0; i < this.length; i++) {
-    rv[`${name}${i}`] = this[i];
+let currentUser = {
+  uid: "1234567890abc",
+  name: "John Doe",
+  priv: "Admin"
+};
+(async function(TConfig){
+  /**
+  * Debug section
+  **/
+  /**
+  *
+  **/
+  let bodyscope;
+  firebase.auth().getRedirectResult().then((result)=>{
+  	let user = result.user;
+  	let credential = result.credential;
+  	if(user != null){
+      if(bodyscope != undefined){
+        bodyscope.loggedIn = true;
+        bodyscope.addUser = function(uid){
+          firebase.database().ref(`users/${uid}`).update({
+            priv: 'User'
+          });
+        };
+        bodyscope.remUser = function(uid){
+          firebase.database().ref(`users/${uid}`).update({
+            priv: 'None'
+          });
+        };
+        bodyscope.downloadFile = function(fileID){
+          firebase.database().ref(`audio/${fileID}`).once('value', (d)=>{
+            let downloadLink = document.createElement('audio');
+            downloadLink.src = Object.values(d.val().data).join('');
+            downloadLink.name = d.val().name;
+            downloadLink.controls = true;
+            document.querySelector(`#${fileID}`).appendChild(downloadLink);
+            document.querySelector(`#${fileID}-generator`).style.display = "none";
+          });
+          firebase.database().ref(`audio/${fileID}`).update({
+            downloads: bodyscope.audioFiles[fileID].downloads+1
+          })
+        };
+        firebase.database().ref('users').on('value', (u)=>{
+          bodyscope.users = u.val();
+        });
+        firebase.database().ref('audio').on('value', (u)=>{
+          bodyscope.audioFiles = u.val();
+        });
+      }
+  		currentUser = firebase.auth().currentUser;
+      let userReference = firebase.database().ref(`users/${currentUser.uid}`);
+      userReference.on('value', (v) => {
+        let data = v.val();
+        for(let kn in data){
+          currentUser[kn] = data[kn];
+        }
+        currentUser.name = currentUser.displayName;
+        if(bodyscope != undefined){
+          bodyscope.currentUser = currentUser;
+        }
+        console.log(v.val?v.val():v);
+      });
+      userReference.update({
+        name: currentUser.displayName,
+        uid: currentUser.uid,
+        online: true
+      });
+  		userReference.onDisconnect().update({
+  			online: false
+  		});
+  	}else{
+      currentUser = {
+        uid: "-1",
+        name: "Anonymous User",
+        priv: "None"
+      };
+    }
+  },(error)=>{
+  	let email = error.email;
+  	let credential = error.credential;
+  });
+  function login(){
+  	var provider;
+  	provider = new firebase.auth.GoogleAuthProvider();
+  	firebase.auth().signInWithRedirect(provider);
   }
-  return rv;
-};
-String.prototype.separate = function(amt) {
-  return this.match(new RegExp(`.{1,${amt}}`, 'g'));
-};
-let Base64 = {
-  // private property
-  _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-  // public method for encoding
-  encode: function(input) {
-    let output = "";
-    let chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-    let i = 0;
-    input = Base64._utf8_encode(input);
-    while (i < input.length) {
-      chr1 = input.charCodeAt(i++);
-      chr2 = input.charCodeAt(i++);
-      chr3 = input.charCodeAt(i++);
-      enc1 = chr1 >> 2;
-      enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-      enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-      enc4 = chr3 & 63;
-      if (isNaN(chr2)) {
-        enc3 = enc4 = 64;
-      } else if (isNaN(chr3)) {
-        enc4 = 64;
-      }
-      output = output +
-        this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-        this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
-    }
-    return output;
-  },
-  // public method for decoding
-  decode: function(input) {
-    var output = "";
-    var chr1, chr2, chr3;
-    var enc1, enc2, enc3, enc4;
-    var i = 0;
-    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-    while (i < input.length) {
-      enc1 = this._keyStr.indexOf(input.charAt(i++));
-      enc2 = this._keyStr.indexOf(input.charAt(i++));
-      enc3 = this._keyStr.indexOf(input.charAt(i++));
-      enc4 = this._keyStr.indexOf(input.charAt(i++));
-      chr1 = (enc1 << 2) | (enc2 >> 4);
-      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-      chr3 = ((enc3 & 3) << 6) | enc4;
-      output = output + String.fromCharCode(chr1);
-      if (enc3 != 64) {
-        output = output + String.fromCharCode(chr2);
-      }
-      if (enc4 != 64) {
-        output = output + String.fromCharCode(chr3);
-      }
-    }
-    output = Base64._utf8_decode(output);
-    return output;
-  },
-  // private method for UTF-8 encoding
-  _utf8_encode: function(string) {
-    string = string.replace(/\r\n/g, "\n");
-    var utftext = "";
-    for (var n = 0; n < string.length; n++) {
-      var c = string.charCodeAt(n);
-      if (c < 128) {
-        utftext += String.fromCharCode(c);
-      } else if ((c > 127) && (c < 2048)) {
-        utftext += String.fromCharCode((c >> 6) | 192);
-        utftext += String.fromCharCode((c & 63) | 128);
-      } else {
-        utftext += String.fromCharCode((c >> 12) | 224);
-        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-        utftext += String.fromCharCode((c & 63) | 128);
-      }
-    }
-    return utftext;
-  },
-  // private method for UTF-8 decoding
-  _utf8_decode: function(utftext) {
-    var string = "";
-    var i = 0;
-    var c = c1 = c2 = 0;
-    while (i < utftext.length) {
-      c = utftext.charCodeAt(i);
-      if (c < 128) {
-        string += String.fromCharCode(c);
-        i++;
-      } else if ((c > 191) && (c < 224)) {
-        c2 = utftext.charCodeAt(i + 1);
-        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-        i += 2;
-      } else {
-        c2 = utftext.charCodeAt(i + 1);
-        c3 = utftext.charCodeAt(i + 2);
-        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-        i += 3;
-      }
-    }
-    return string;
+  function logout(){
+  	firebase.database().ref(`users/${currentUser.uid}`).update({
+  		online: false
+  	});
+  	firebase.auth().signOut().then(()=>{
+  		location.reload();
+  	}).catch((error)=>{
+  	  // An error happened.
+  		console.log(error);
+  		alert("Somehow you screwed up logging out.");
+  	});
   }
-};
-function setupAudio(audioBase64){
-  console.log(audioBase64)
-}
-var fileInput = document.querySelector('input[type="file"]');
-fileInput.addEventListener('change', function(e) {
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    setupAudio(this.result);
+  Array.prototype.multipart = function(name) {
+    let rv = {};
+    for (let i = 0; i < this.length; i++) {
+      rv[`${name}${i}`] = this[i];
+    }
+    return rv;
   };
-  reader.readAsDataURL(this.files[0]);
-}, false);
+  String.prototype.separate = function(amt) {
+    return this.match(new RegExp(`.{1,${amt}}`, 'g'));
+  };
+  var app = angular.module('page', []);
+  app.controller("body", [
+    '$scope',
+    function($scope){
+      bodyscope = $scope;
+      $scope.loggedIn = false;
+      $scope.website_title = TConfig.title;
+      $scope.currentUser = currentUser;
+      $scope.view = 'previewFiles';
+      $scope.setView = function(_view){
+        $scope.view = _view;
+      }
+    }
+  ]);
+  function setupAudio(player, audioBase64, fileName){
+    let dataset = audioBase64.separate(200000).multipart('data');
+    player.src = audioBase64;
+    console.log(dataset.data0.length)
+    if(bodyscope != undefined){
+      bodyscope.addAudio = function(){
+        let newKey = firebase.database().ref('audio').push().key;
+        let newAudio = {};
+        newAudio[newKey] = {
+          name: fileName,
+          downloads: 0,
+          index: newKey
+        };
+        firebase.database().ref('audio').update(newAudio);
+        for(let dv in dataset){
+          let d = {};
+          d[dv] = dataset[dv];
+          firebase.database().ref(`audio/${newKey}/data`).update(d);
+        }
+      };
+    }
+  }
+  window.onload = function(){
+    bodyscope.loggedIn = false;
+    setInterval(()=>{
+      if(document.querySelector("#lgin")){
+        document.querySelector("#lgin").removeEventListener('click', login);
+        document.querySelector("#lgin").addEventListener('click', login);
+      }else if(document.querySelector("#lgout")){
+        document.querySelector("#lgout").removeEventListener('click', logout);
+        document.querySelector("#lgout").addEventListener('click', logout);
+      }
+      if(document.querySelector('input[type="file"]')){
+        let fileInput = document.querySelector('input[type="file"]');
+        let _audioPreview_ = document.querySelector('#audioPreview');
+        function fileInputListener(e) {
+          var reader = new FileReader();
+          reader.onload = function(e) {
+            setupAudio(_audioPreview_, this.result, fileInput.files[0].name);
+          };
+          reader.readAsDataURL(this.files[0]);
+        }
+        fileInput.removeEventListener('change', fileInputListener, false);
+        fileInput.addEventListener('change', fileInputListener, false);
+      }
+      bodyscope.$apply();
+    }, 1000);
+  };
+})({
+  title: "History"
+});
